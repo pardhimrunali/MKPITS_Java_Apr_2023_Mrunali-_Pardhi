4e0f5c15cefb0ca064ebbcb7edaa6717393ea8f6
Mrunali <mrunali25pardhi@gmail.com>
2023-04-14 22:12:21 +0530

    Add

---
+++ C_programming/ADD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n,n2,sum;
+  printf("enter two numbers");
+  scanf("%d%d",&n,&n2);
+  sum=n+n2;
+  printf("\n sum of two numbers is %d",sum);
+
+  getch();
+}
