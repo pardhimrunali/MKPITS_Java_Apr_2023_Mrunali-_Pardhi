d6f96b7663a932998025b42f4c31dcd5c5643b9d
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 17:23:23 +0530

    volcuboid

---
+++ C_programming/VCUBOID.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int l,b,h,Volume;
+ printf("\n Enter the value of length,breadth,height");
+ scanf("%d%d%d",&l,&b,&h);
+ Volume=l*b*h;
+ printf("\n volume of cuboid is %d",Volume);
+ getch();
+}
