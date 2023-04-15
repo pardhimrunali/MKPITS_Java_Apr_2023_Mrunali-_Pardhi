848838fd102d0595c75b37ab37678c54ce6c7e9b
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 16:18:56 +0530

    Divide

---
+++ C_programming/DIVIDE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  float n1,n2,div;
+  printf("Enter the value of two numbers");
+  scanf("%f%f",&n1,&n2);
+  div=n1/n2;
+  printf("\n division value is %f",div);
+  getch();
+}
