c42b1884f9db869263f165e1b363cf5d918cc2cf
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 18:04:13 +0530

    perimsquare

---
+++ C_programming/PERISQU.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int side,perimeter;
+  printf("\n Enter the value of side");
+  scanf("%d",&side);
+  perimeter=4*side;
+  printf("\n perimeter of square is %d",perimeter);
+  getch();
+}
