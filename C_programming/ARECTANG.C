62f410788973713d756a0ab91dc5f5c1b3a5e6c1
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 17:15:16 +0530

    Areactangle

---
+++ C_programming/ARECTANG.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int l,b,Area;
+  printf("\n Enter the value of length");
+  scanf("%d",&l);
+  printf("\n Enter the value of breadth");
+  scanf("%d",&b);
+  Area=l*b;
+  printf("\n Area of rectangle is %d",Area);
+  getch();
+}
