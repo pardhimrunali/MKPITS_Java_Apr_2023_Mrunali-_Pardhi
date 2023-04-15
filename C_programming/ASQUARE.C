f96712e7cef3e98636f99b19e629d31daef633d5
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 17:34:42 +0530

    Asquare

---
+++ C_programming/ASQUARE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int side,Area;
+  printf("Enter the value of side");
+  scanf("%d",&side);
+  Area=side*side;
+  printf("\n Area of sqaure is %d",Area);
+  getch();
+}
