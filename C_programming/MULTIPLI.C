b901f1ecc0465307132f85fdae69e94f4fcc0373
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 16:01:30 +0530

    multiplication

---
+++ C_programming/MULTIPLI.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n1,n2,mul;
+  printf("\n enter the value of n1");
+  scanf("%d",&n1);
+  printf("\n Enter the value of n2");
+  scanf("%d",&n2);
+  mul=n1*n2;
+  printf("\n multiplication of two number is %d",mul);
+  getch();
+}
