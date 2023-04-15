bb1539341861d16fb455cda1ed1863785f257a3e
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 18:30:40 +0530

    Perimequt

---
+++ C_programming/PERIMEQU.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int side,perimeter;
+  printf("\n Enter the value of side");
+  scanf("%d",&side);
+  perimeter=3*side;
+  printf("\n perimeter of equilateral triangle is %d",perimeter);
+  getch();
+}
