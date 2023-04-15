9e05161f9d013a8aa4551612bd999be590d6d78d
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 18:20:14 +0530

    circumfcircle

---
+++ C_programming/CIRCUMFC.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  float d,pi=3.14,circumf;
+  printf("\n Enter the value of diameter");
+  scanf("%f",&d);
+  circumf=pi*d;
+  printf("\n Circumference of circle is %f",circumf);
+  getch();
+}
