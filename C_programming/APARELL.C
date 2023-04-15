b44ceb98128d7bbdd3f4fcaa02f997a5cd30beba
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 17:53:19 +0530

    Aparallelogram

---
+++ C_programming/APARELL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int b,h,Area;
+ printf("\n Enter the value of base");
+ scanf("%d",&b);
+ printf("\n Enter the value of height");
+ scanf("%d",&h);
+ Area=b*h;
+ printf("\n Area of parellelogram is %d",Area);
+ getch();
+}
