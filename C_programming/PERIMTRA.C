98ec83363c074c70aab241bc2c07170a8917d76b
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 18:40:16 +0530

    Perimtrapezoid

---
+++ C_programming/PERIMTRA.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int s1,s2,s3,s4,perimt;
+   printf("\n Enter the value of s1,s2,s3,s4");
+   scanf("%d%d%d%d",&s1,&s2,&s3,&s4);
+   perimt=s1+s2+s3+s4;
+   printf("\n Perimeter of trapezoid is %d",perimt);
+   getch();
+}
