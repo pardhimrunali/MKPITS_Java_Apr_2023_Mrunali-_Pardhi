45ba7db800b4fb5056de58c3352d9039a3c8f4dc
Mrunali <mrunali25pardhi@gmail.com>
2023-04-15 18:47:52 +0530

    ARhombus

---
+++ C_programming/ARHOMBUS.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  float d1,d2,Area;
+  printf("\n Enter the value of d1 and d2");
+  scanf("%f%f",&d1,&d2);
+  Area=(d1*d2)/2;
+  printf("\n Area of rhombus is %f",Area);
+  getch();
+}
