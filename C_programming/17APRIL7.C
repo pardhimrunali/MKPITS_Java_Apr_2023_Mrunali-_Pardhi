d8d6ab25b6fbb2db457f9853cafd719bb2408413
Mrunali <mrunali25pardhi@gmail.com>
2023-04-18 20:13:47 +0530

    AP rectangle

---
+++ C_programming/17APRIL7.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int width=5,height=7,area,perimeter;
+  area=width*height;
+  printf("\n area of rectangle is %d",area);
+  perimeter=2*(width+height);
+  printf("\n perimeter of rectangle is %d",perimeter);
+  getch();
+}
