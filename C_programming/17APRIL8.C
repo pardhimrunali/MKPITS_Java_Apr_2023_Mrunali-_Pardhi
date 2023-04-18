8cca75a9ccff55683d7c7e4e6485bbad52b13fe7
Mrunali <mrunali25pardhi@gmail.com>
2023-04-18 20:20:17 +0530

    AP circle

---
+++ C_programming/17APRIL8.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  float pi=3.14,r=6,Area,perim;
+  Area=pi*r*r;
+  printf("\n area of circle is %f",Area);
+  perim=2*pi*r;
+  printf("\n perimeter of circle is %f",perim);
+  getch();
+}
