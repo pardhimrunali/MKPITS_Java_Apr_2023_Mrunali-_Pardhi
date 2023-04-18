8512dfc3e073eb16554597ba4f16e0710be063b3
Mrunali <mrunali25pardhi@gmail.com>
2023-04-18 19:27:17 +0530

    PRINT F C

---
+++ C_programming/17APRILL.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  printf("\t#######\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#####\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\n");
+  printf("\t######\n");
+  printf("\t######\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#\n");
+  printf("\t#####\n");
+
+  getch();
+}
