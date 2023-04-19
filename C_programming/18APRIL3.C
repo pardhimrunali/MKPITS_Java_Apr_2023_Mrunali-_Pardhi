5143b2fb0547c34cf82d04d7aefbff710816cdbc
Mrunali <mrunali25pardhi@gmail.com>
2023-04-19 16:50:21 +0530

    Largest no

---
+++ C_programming/18APRIL3.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,c;
+  printf("\n Enter the three numbers");
+  scanf("%d%d%d",&a,&b,&c);
+  if(a>=b&&a>=c)
+  printf("%d is the largest number",a);
+  else
+      if (b>=c&&b>=a)
+      printf("%d is the largest number",b);
+  else
+     printf("%d is the largest number",c);
+
+     getch();
+}
