8ca20d7ce137d6814f9818caad55664980b1b3e0
Mrunali <mrunali25pardhi@gmail.com>
2023-04-22 20:25:15 +0530

    Even No

---
+++ C_programming/19APRIL6.C
@@ -0,0 +1,17 @@
+	#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+   int i;
+   printf("\n Even numbers betweeen 1 to 50");
+   for(i=1 ;i<=50 ;i++)
+   {
+     if(i%2==0)
+     {
+       printf("\n%d",i);
+
+     }
+    }
+    getch();
+   }
