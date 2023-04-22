17a106295371fdcb1409a25cdae37ad35d5a18f6
Mrunali <mrunali25pardhi@gmail.com>
2023-04-22 15:20:21 +0530

    Month of the year

---
+++ C_programming/19APRIL5.C
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+       int num1;
+       clrscr();
+       printf("\n Enter the numbers between 1 to 12");
+       scanf("%d",&num1);
+       if(num1==1)
+       {
+	printf("\n january");
+       }
+       if(num1==2)
+       {
+	 printf("\n February");
+       }
+       if(num1==3)
+       {
+	 printf("\n March");
+       }
+       if(num1==4)
+       {
+	  printf("\n April");
+       }
+       if (num1==5)
+       {
+	 printf("\n May");
+       }
+       if(num1==6)
+       {
+	 printf("\n June");
+       }
+       if(num1==7)
+       {
+	printf("\n July");
+       }
+       if(num1==8)
+       {
+	  printf("\n August");
+       }
+       if(num1==9)
+       {
+	  printf("\n September");
+       }
+       if(num1==10)
+       {
+	 printf("\n Octomber");
+       }
+       if(num1==11)
+	 {
+	   printf("\n November");
+	 }
+       if(num1==12)
+       {
+	  printf("\n December");
+       }
+       getch();
+}
