30bcade319b404f473f703a0adc373a9be821d76
Mrunali <mrunali25pardhi@gmail.com>
2023-04-25 16:35:36 +0530

    ABCDE

---
+++ C_programming/24APRIL6.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int r,c;
+   clrscr();
+   for(r=1;r<5;r++)
+   {
+      for(c='A';c='E';c++)
+      {
+	 printf("%c",c);
+      }
+   }
+   getch();
+}
