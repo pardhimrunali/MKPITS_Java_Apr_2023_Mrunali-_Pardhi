ee262804684ca825611e0b45c129bff79908f8f0
Mrunali <mrunali25pardhi@gmail.com>
2023-04-27 14:09:56 +0530

    55555

---
+++ C_programming/24APRIL7.C
@@ -0,0 +1,24 @@
+//55555
+//44444
+//33333
+//22222
+//11111
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int r,c;
+     clrscr();
+     for(r=5;r>=1;r--)
+     {
+       for(c=5;c>=1;c--)
+       {
+	   printf("%d",r);
+       }
+       printf("\n");
+
+     }
+     getch();
+}
