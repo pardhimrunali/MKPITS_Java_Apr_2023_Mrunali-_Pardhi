b32c5a1477192cf1f8f240b60029df9184333b81
Mrunali <mrunali25pardhi@gmail.com>
2023-04-29 14:10:52 +0530

    max no argument no returntype

---
+++ C_programming/28APRIL2.C
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+
+
+void max();
+
+
+
+void main()
+{
+
+
+     max();
+     getch();
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+void max()
+{
+
+
+     int a,b;
+
+
+
+
+     printf("Enter two numbers");
+     scanf("%d%d",&a,&b);
+     if(a>b)
+     {
+	 printf("max is %d",a);
+     }
+     else
+     {
+       printf("max is %d",b);
+     }
+
+
+
+
+
+}
+
+
+
+
