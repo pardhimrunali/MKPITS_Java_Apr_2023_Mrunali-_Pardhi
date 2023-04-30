f81086711915c79b3bb58ebf107bdad934e66ce6
Mrunali <mrunali25pardhi@gmail.com>
2023-05-01 02:54:21 +0530

    edited celsius to farenheit function with no argumnet & no returntype

---
+++ C_programming/76.C
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<conio.h>
+// celsius to ferenheit	function with no argument & no returntype
+void cf();
+void main()
+{
+  clrscr();
+  cf();
+  getch();
+
+}
+void cf()
+{
+       float c,f;
+       printf("Enter the celsius temperature");
+       scanf("%f",&c);
+       f=(c*9/5)+32;
+       printf("Ferenheit temperature is %f",f);
+}
