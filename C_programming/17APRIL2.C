c021e44c0a2dd9fb61d903260e873a6c6d7bbc74
pardhimrunali <130438487+pardhimrunali@users.noreply.github.com>
2023-05-09 14:30:51 +0530

    Update 17APRIL2.C

--- C_programming/17APRIL2.C
+++ C_programming/17APRIL2.C
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-  printf("\n Name:Mrunali A. Pardhi");
+  printf("\n Name:Mrunali Arunkumar Pardhi");
   printf("\n DOB:April 30,2000");
   printf("\n Mobile:7249536491");
   getch();
