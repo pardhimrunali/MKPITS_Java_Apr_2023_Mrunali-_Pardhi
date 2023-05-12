a99884e027925894da28eb842c3f69b4dbac7cf5
Mrunali <mrunali25pardhi@gmail.com>
2023-05-13 00:14:28 +0530

    EEEEE RIGHT ANGLE TRIANGLE

---
+++ C_programming/25APRIL8.C
@@ -0,0 +1,34 @@
+//EEEEE
+//DDDD
+//CCC
+//BB
+//A
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int r,c;
+  clrscr();
+  for(r='E';r>='A';r--)
+  {
+    for(c='A';c<=r;c++)
+    {
+      printf("%c",r);
+    }
+    printf("\n");
+  }
+  getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
