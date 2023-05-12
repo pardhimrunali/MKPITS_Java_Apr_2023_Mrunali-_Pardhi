374873331d4ee297c69261846cc07f73d57d7b4f
Mrunali <mrunali25pardhi@gmail.com>
2023-05-12 22:24:06 +0530

    12345

---
+++ C_programming/25APRIL5.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+//12345
+//1234
+//123
+//12
+//1
+void main()
+{
+  int r,c;
+  clrscr();
+  for(r=5;r>=1;r--)
+  {
+    for(c=1;c<=r;c++)
+    {
+      printf("%d",c);
+    }
+    printf("\n");
+  }
+  getch();
+}
