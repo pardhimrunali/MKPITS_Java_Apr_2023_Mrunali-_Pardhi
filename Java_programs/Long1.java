34b1ed90bdd5027ac90e2f47cfc9c481bfb7c55b
Mrunali <mrunali25pardhi@gmail.com>
2023-05-30 10:30:16 +0530

    Long

---
+++ Java_programs/Long1.java
@@ -0,0 +1,16 @@
+class Long{
+               public long addLong(long number1,long number2)
+			   {
+			      return number1 + number2;
+	       }
+		   
+}
+class Long1{
+             public static void main(String[] args)
+			 {
+			 
+			    Long long2 = new Long();
+		        long ans = long2.addLong(200,44);
+		        System.out.println(ans);
+	}
+}
