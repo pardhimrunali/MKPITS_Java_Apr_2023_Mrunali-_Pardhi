1cec482cb8a5ebd5307bff190db48f7aa125b005
Mrunali <mrunali25pardhi@gmail.com>
2023-06-25 23:59:53 +0530

    Do while loop example

---
+++ Java_programs/Dowhile loop example/Hotspot.java
@@ -0,0 +1,14 @@
+public class Hotspot {
+
+    int device=5;
+    public void HotspotConnection(){
+    do{
+        System.out.println("device is connected");
+        device--;
+      }
+    while(device!=0);
+        System.out.println("Device limit is over");
+
+    }
+
+}
---
+++ Java_programs/Dowhile loop example/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Hotspot device = new Hotspot();
+        device.HotspotConnection();
+
+
+    }
+}
