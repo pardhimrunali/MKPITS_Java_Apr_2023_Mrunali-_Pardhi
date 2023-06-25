a011e35da66b3acdb6f2b45ce2ff8e6a970bdfee
Mrunali <mrunali25pardhi@gmail.com>
2023-06-25 23:57:30 +0530

    While loop example

---
+++ Java_programs/Whileloop/NoticeForEmployee.java
@@ -0,0 +1,9 @@
+public class NoticeForEmployee {
+    int employeeid=100;
+    public void NoticeToEmployee(){
+        while(employeeid!=0){
+            System.out.println("Send The Notice");
+            employeeid--;
+        }
+    }
+}
