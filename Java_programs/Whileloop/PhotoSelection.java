c23c2fed921f9253c3bd76f64c435289bc58c4cc
Mrunali <mrunali25pardhi@gmail.com>
2023-06-26 14:53:22 +0530

    While loop example

---
+++ Java_programs/Whileloop/PhotoSelection.java
@@ -0,0 +1,11 @@
+public class PhotoSelection {
+    int Photo=50;
+public void photoLimit(){
+    while(Photo!=0)
+    {
+        System.out.println("selection of Photo Continue");
+        Photo--;
+    }
+    System.out.println("Sharing option will not show");
+}
+}
