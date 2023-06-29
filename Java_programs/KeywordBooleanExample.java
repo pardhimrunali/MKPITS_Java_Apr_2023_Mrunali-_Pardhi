0f7a2c69ed8ace3cead00bd4091ee0ee79b4c03f
Mrunali <mrunali25pardhi@gmail.com>
2023-06-30 01:42:47 +0530

    Boolean keyword example

---
+++ Java_programs/KeywordBooleanExample.java
@@ -0,0 +1,17 @@
+public class KeywordBooleanExample {
+    public static void main (String args[])
+    {
+        int numberFirst=5;
+        int numberSecond=200;
+        boolean resultforcorrect=true;
+        boolean result=false;
+        if(numberFirst<numberSecond)
+        {
+            System.out.println("Number first is less than numbersecond is "+resultforcorrect);
+        }
+        else{
+            System.out.println("Number First is less than number second is " +result);
+        }
+    }
+
+}
---
+++ Java_programs/PatternFirst.java
@@ -0,0 +1,8 @@
+public class PatternFirst{
+public static void main(String args[])
+{
+int number=5;
+for(int row=5;row<=)
+}
+
+}
