b71de04a4e01e62785cd29bb003da0312ad3a37d
Mrunali <mrunali25pardhi@gmail.com>
2023-06-30 23:09:00 +0530

    Java programs

---
+++ Java_programs/AdditionExample.java
@@ -0,0 +1,12 @@
+public class AdditionExample{
+public static void main(String argos[])
+{
+
+long numberFirst=5;
+long numberSecond=7;
+long sum=(numberFirst+numberSecond);
+System.out.println(sum);
+
+}
+
+}
---
+++ Java_programs/MultiplicationExample.java
@@ -0,0 +1,12 @@
+public class MultiplicationExample{
+public static void main(String argos[])
+{
+
+byte numberFirst=5;
+byte numberSecond=7;
+int multiplication=(numberFirst+numberSecond);
+System.out.println(multiplication);
+
+}
+
+}
