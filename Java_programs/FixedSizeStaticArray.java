36a14d4dda18bf1e8076bc874096569f071ec400
Mrunali <mrunali25pardhi@gmail.com>
2023-07-05 00:22:17 +0530

    Print the value of array elements

---
+++ Java_programs/FixedSizeStaticArray.java
@@ -0,0 +1,38 @@
+/**<h1> Print the elements</h1>
+*FixedSizeStaticArray program print the value of give array as a output.
+@author Mrunali Pardhi
+@since 05/07/23
+*/
+
+/**
+*Algorithm to print an array elements
+*step 1- declare datatype of an array 
+*step 2- give the size to an array
+*step 3- enetr the data in array position
+*step 4- use for loop to print multiple value
+*step 5- write the print statement to print values
+**/
+class FixedSizeStaticArray{
+
+public static void main(String args[])
+{
+
+// declare datatype and name of an variable and size of the array
+   int array[]=new int[4];
+   
+// enter data in array 
+array[0]=20;
+array[1]=40;
+array[2]=80;
+array[3]=70;
+
+for(int count=0;count<5;count++){
+
+System.out.println(array[count]);
+
+}
+   
+
+}
+
+}
--- Java_programs/PateernOfRightAngleTriangle.java
+++ Java_programs/PateernOfRightAngleTriangle.java
@@ -1,4 +1,8 @@
-
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
 
 public class PateernOfRightAngleTriangle{
 public static void main(String[] args){
