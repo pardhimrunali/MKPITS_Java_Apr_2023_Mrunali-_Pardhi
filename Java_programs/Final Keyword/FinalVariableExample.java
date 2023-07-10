99a0a13e51ede22a0112cbf435eccd8955b3a3b5
Mrunali <mrunali25pardhi@gmail.com>
2023-07-10 21:42:23 +0530

    Final keyword used in variable example

---
+++ Java_programs/Final Keyword/FinalVariableExample.java
@@ -0,0 +1,8 @@
+public class FinalVariableExample{
+public static void main(String args[])
+{
+FinalVariableMethod finalvariable=new FinalVariableMethod();
+finalvariable.markCalculate();
+}
+
+}
---
+++ Java_programs/Final Keyword/FinalVariableMethod.java
@@ -0,0 +1,13 @@
+public class FinalVariableMethod{
+
+ final int totalmarks=100;
+void markCalculate(){
+
+  int mathmarks=47;
+  totalmarks=200;
+  int percentageinmath=mathmarks/totalmarks;
+  
+
+}
+
+}
