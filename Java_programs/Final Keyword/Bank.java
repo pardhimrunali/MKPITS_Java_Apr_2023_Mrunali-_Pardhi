fc4b66ceb20dc670601c21f8c272e7b5c01a3801
Mrunali <mrunali25pardhi@gmail.com>
2023-07-11 00:00:19 +0530

    final class used in Example

---
+++ Java_programs/Final Keyword/Bank.java
@@ -0,0 +1,7 @@
+ final class Bank{
+void atmPassword(){
+int password=4528;
+System.out.println(password);
+}
+
+}
---
+++ Java_programs/Final Keyword/Main.java
@@ -0,0 +1,13 @@
+public class Main extends Bank{
+  void atmPassword(){
+int password=4529;
+System.out.println(password);
+}
+
+public static void main(String args[])
+{
+  Main passwordofatm=new Main();
+  passwordofatm.atmPassword();
+
+}
+}
