60cd571a6743c498f9b862003a98329ba2798d65
Mrunali <mrunali25pardhi@gmail.com>
2023-08-08 03:45:09 +0530

    Math class Methods

---
+++ Java_programs/Math class Methods/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       double firstNumber=20;
+       double secondNumber=4;
+
+        System.out.println("1.Maximum of two numbers is  "+Math.max(firstNumber,secondNumber));
+        System.out.println("2.Minimum of two numbers is  "+Math.min(firstNumber,secondNumber));
+        System.out.println("3.Squareroot of numbers is  "+Math.sqrt(secondNumber));
+        //power of method 20*20*20*20 return 160000
+        System.out.println("4. Power of firstnumber and secondnumber is  "+Math.pow(firstNumber,secondNumber));
+        System.out.println("5.Logarithmic value of secondnumber is  "+Math.log(secondNumber));
+
+    }
+}
