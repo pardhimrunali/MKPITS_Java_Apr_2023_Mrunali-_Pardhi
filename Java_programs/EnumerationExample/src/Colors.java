0ebbebdb26b12eefe5e05206cd8ba606cc3b1166
Mrunali <mrunali25pardhi@gmail.com>
2023-08-15 03:38:07 +0530

    enumeration_color_example

---
+++ Java_programs/EnumerationExample/src/Colors.java
@@ -0,0 +1,16 @@
+public class Colors {
+    enum color{red,blue,pink,purple,green};
+    public  static void main(String[] args)
+    {
+        for(color c:color.values())//values() method
+            System.out.println(c);git 
+        {
+
+            //valuesOf() method
+            System.out.println("Value of red is:"+color.valueOf("red"));
+       //ordinal() method
+            System.out.println("Index of green is:"+color.valueOf("green").ordinal());
+
+        }
+    }
+}
