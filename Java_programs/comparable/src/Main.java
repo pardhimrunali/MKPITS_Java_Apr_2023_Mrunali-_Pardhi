81a201b1617819bf7213d14a7e46d38093ecd4bd
Mrunali <mrunali25pardhi@gmail.com>
2023-08-29 17:49:55 +0530

    Comparable Interface

---
+++ Java_programs/comparable/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Student> data=new LinkedList<Student>();
+        Student[] students=new Student[4];
+        students[0]=new Student("bru",50);
+        students[1]=new Student("arun",55);
+        students[2]=new Student("crunal",10);
+        students[3]=new Student("pk",80);
+
+data.addAll(Arrays.asList(students));
+        Collections.sort(data);
+        for (Student p:data
+             ) {
+            System.out.println(p.getName()+" "+p.getMarks());
+
+        }
+
+    }
+}
---
+++ Java_programs/comparable/src/Student.java
@@ -0,0 +1,36 @@
+public class Student  implements Comparable<Student> {
+
+    private String name;
+    private int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public String toString() {
+
+        return name + marks;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(marks==o.marks)
+            return 0;
+                    else if(marks<o.marks)
+                    return -1;
+                    else{
+            return 1;
+        }
+
+    }
+}
+
