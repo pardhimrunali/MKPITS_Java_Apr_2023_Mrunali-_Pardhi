f543143461370f4785eff4085bcf79d1789ac57d
Mrunali <mrunali25pardhi@gmail.com>
2023-09-12 10:39:48 +0530

    Sortmap values using comparator

---
+++ Java_programs/SortHahmap_values_using_comparator/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main implements Comparator {
+    public static void main(String[] args) {
+        HashMap<String,Integer> data=new HashMap<String,Integer>();
+        data.put("ram",45);
+        data.put("shyam",65);
+        data.put("saba",10);
+
+        TreeMap<String,Integer> s=new TreeMap<>(data);
+        System.out.println(s);
+
+        List <Integer> p= new List<Integer>(s) {
+
+
+
+
+        }
+    }
