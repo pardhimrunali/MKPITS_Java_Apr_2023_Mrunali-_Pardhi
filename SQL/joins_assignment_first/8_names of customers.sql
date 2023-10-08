847191e97f6e5503bedeb1d600af29cb7bba3036
Mrunali <mrunali25pardhi@gmail.com>
2023-10-09 02:32:10 +0530

    8_List the names of customers who have rented the film titled Alien Center

---
+++ SQL/joins_assignment_first/8_names of customers.sql
@@ -0,0 +1,10 @@
+/*List the names of customers who have rented the film titled "Alien Center".*/
+select* from customer /*customerid*/
+select* from rental /*customerid*/ /*store_id*/
+select* from inventory  /*store_id*/ /*film id*/
+select* from film /*film id*/
+
+select first_name,last_name from customer join rental on
+customer.customer_id=rental.customer_id join inventory
+on inventory.store_id=customer.store_id join film 
+on film.film_id=inventory.film_id where title='Alien Center'
