6b6064db1ad9031eaa17ec1ffa53f89ed5fff78f
Mrunali <mrunali25pardhi@gmail.com>
2023-10-10 17:19:11 +0530

    Check_number_is_even_using_js_function

---
+++ HTML.CSS.JS/check_number_even.js
@@ -0,0 +1,14 @@
+function check_even_number()
+{
+    const values=[12,5,78];
+    for(var count=0;count<values.length;count++)
+    {
+        if(count%2==0)
+        {
+            document.write("nuber are even"+values[count]+"<br>");
+        }
+        
+        
+    }
+    
+}
