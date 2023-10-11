af2f669572d095147df57fa02a812d7bf65f0cb0
Mrunali <mrunali25pardhi@gmail.com>
2023-10-11 23:19:04 +0530

    32_Retrieve the film titles rented by the customer with customer_id 200

---
+++ SQL/joins_assignment_first/32_customer_with_customer_id 200.sql
@@ -0,0 +1,7 @@
+/*32.Retrieve the film titles rented by the customer with customer_id 200*/
+
+select title from film join inventory
+on film.film_id=inventory.film_id join rental on
+rental.inventory_id=inventory.inventory_id join customer
+on customer.customer_id=rental.customer_id where customer.customer_id=200
+ 
