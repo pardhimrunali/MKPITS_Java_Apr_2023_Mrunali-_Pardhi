92753fa48b8cf55e556c9f2fcf9b82e177bf1615
Mrunali <mrunali25pardhi@gmail.com>
2023-10-13 22:04:14 +0530

    25_List the rental date and return date for all rentals made by   customer Susan Johnson

---
+++ SQL/joins_assignment_first/25_List_the_rental_date.sql
@@ -0,0 +1,11 @@
+
+ 
+ /*25List the rental date and return date for all rentals made by 
+  customer "Susan Johnson".*/
+    select * from customer
+	select * from rental 
+    
+	select rental_date,return_date from rental join customer 
+    on customer.customer_id=rental.customer_id where first_name='CHARLOTTE' and
+    last_name='HUNTER'
+ 
