39d09d0443a543898f7ca27165052826748f8c91
Mrunali <mrunali25pardhi@gmail.com>
2023-10-13 21:30:05 +0530

    20_Display the rental date and rental duration for each rental made by customer  Karen Smith

---
+++ SQL/joins_assignment_first/20_rental_duration.sql
@@ -0,0 +1,13 @@
+/*20 Display the rental date and rental duration for each rental made by customer
+ -- "Karen Smith".*/
+
+    select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+ select rental_date,rental_duration from rental join customer 
+ on customer.customer_id=rental.customer_id join inventory on
+ inventory.inventory_id=rental.inventory_id join film on 
+ film.film_id=inventory.film_id 
+ 
+  
