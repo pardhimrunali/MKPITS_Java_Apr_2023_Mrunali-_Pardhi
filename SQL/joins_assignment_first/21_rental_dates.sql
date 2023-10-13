a1b33b47c758b0fcaf269fc9b23a5979a016d348
Mrunali <mrunali25pardhi@gmail.com>
2023-10-13 21:50:34 +0530

    21_List the film titles and rental dates for all rentals made by customer Steven Taylor

---
+++ SQL/joins_assignment_first/21_rental_dates.sql
@@ -0,0 +1,15 @@
+
+ 
+/*21List the film titles and rental dates for all rentals made by customer 
+ -- "Steven Taylor".*/
+    select * from customer
+	select * from rental 
+	select * from inventory 
+    select * from film
+    
+ select title,rental_date from film join inventory on
+ inventory.film_id=film.film_id join rental on 
+ rental.inventory_id=inventory.inventory_id join customer on
+ customer.customer_id=rental.customer_id where first_name='Steven' and last_name='Taylor'
+ 
+  
