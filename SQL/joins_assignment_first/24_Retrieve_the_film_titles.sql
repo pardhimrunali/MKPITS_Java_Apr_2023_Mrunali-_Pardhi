02c2081c0f59a13b28560ba765e20a61515d2966
Mrunali <mrunali25pardhi@gmail.com>
2023-10-13 22:02:11 +0530

    24_Retrieve the film titles and rental dates for all rentals made by the   customer with customer_id 600.

---
+++ SQL/joins_assignment_first/24_Retrieve_the_film_titles.sql
@@ -0,0 +1,15 @@
+
+ 
+ /*24 Retrieve the film titles and rental dates for all rentals made by the
+  customer with customer_id 600.*/
+    select* from customer
+	select* from rental 
+	select*from inventory 
+    select* from film
+    
+select title,rental_date from film join inventory   
+on film.film_id=inventory.film_id join rental  
+on rental.inventory_id=inventory.inventory_id join customer 
+ on customer.customer_id=rental.customer_id where customer.customer_id=600 
+ 
+  
