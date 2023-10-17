f560e18b21bea14e3e7719edb303dd2b760c75a1
Mrunali <mrunali25pardhi@gmail.com>
2023-10-17 16:14:10 +0530

    10_group_by_Calculate the count of rentals for each film language

---
+++ SQL/group_by_assignment/10_count_of_rentals.sql
@@ -0,0 +1,7 @@
+/*10. Calculate the count of rentals for each film language.*/
+
+use sakila
+
+select language_id,count(rental_id) from film join inventory
+on film.film_id=inventory.film_id join rental on
+rental.inventory_id=inventory.inventory_id group by language_id
