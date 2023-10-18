92b9bfb213ecd3eb371f5562e5388d0aa359aed1
Mrunali <mrunali25pardhi@gmail.com>
2023-10-18 14:30:22 +0530

    trigger_creation

---
+++ SQL/Trigger/after_trigger_creation.sql
@@ -0,0 +1,20 @@
+use product_management
+select * from products
+DELIMITER //
+CREATE TRIGGER afte_sales_trigger
+AFTER INSERT ON sales FOR EACH ROW
+BEGIN
+
+
+
+
+
+UPDATE products
+    set quantity=quantity-NEW.quantity_sale
+    where product_id=NEW.product_id;
+END;
+//
+DELIMITER ;
+
+insert into sales values(101,01,25)
+
