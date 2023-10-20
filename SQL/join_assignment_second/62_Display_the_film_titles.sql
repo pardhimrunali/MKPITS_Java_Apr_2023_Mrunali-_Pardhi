a1de9c15e1ef7654e46bbaac3be4a87d4491727c
Mrunali <mrunali25pardhi@gmail.com>
2023-10-20 20:35:25 +0530

    62_Display the film titles rented by the customer with customer_id 160

---
+++ SQL/join_assignment_second/62_Display_the_film_titles.sql
@@ -0,0 +1,15 @@
+/*62.	Display the film titles rented by the customer with customer_id 160.*/
+
+select title
+ from
+ film 
+join 
+inventory on
+film.film_id=inventory.film_id
+join
+rental on
+inventory.inventory_id=rental.inventory_id 
+join customer
+on
+rental.customer_id=customer.customer_id
+where customer.customer_id=160
