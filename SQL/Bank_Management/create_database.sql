5cd059d1d9ecbf3fb24c93df9ec2ff969854f76a
Mrunali <mrunali25pardhi@gmail.com>
2023-10-27 23:26:18 +0530

    HttpSession_interface_methods_example

--- SQL/Bank_Management/create_database.sql
+++ SQL/Bank_Management/create_database.sql
@@ -3,5 +3,12 @@ use bankManegement
 create table account(user_id varchar(10) primary key,user_password varchar(10),
 user_name varchar(20),address varchar(30),city varchar(20),email_id varchar(30),balance double)
 
-drop table account
+
 select * from account
+constraint f_k foreign key(Roll_no) references Student_details(Roll_no))
+
+create  table transactions(user_id varchar(10),transaction_date date,
+user_amount double,transactiontype varchar(20),
+constraint f_k foreign key(user_id) references account(user_id)   )
+
+select * from transactions
