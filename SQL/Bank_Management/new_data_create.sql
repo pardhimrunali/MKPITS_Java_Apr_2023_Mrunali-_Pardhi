a242259356cdf7e384a78fb8ce0f97ed3977dd13
unknown <mrunali25pardhi@gmail.com>
2023-11-16 20:12:02 +0530

    Bank_project_jsp_mvc

--- SQL/Bank_Management/new_data_create.sql
+++ SQL/Bank_Management/new_data_create.sql
@@ -10,10 +10,25 @@ create  table transactions(user_id varchar(10),transaction_date date,
 user_amount double,transactiontype varchar(20),
 constraint f_k foreign key(user_id) references account(user_id))
 
+select * from accountaccount
 select * from transactions
+
+/*-------add new column in account----*/
+alter table account add created_on Date
+alter table account add is_approved varchar(10)  default 'false'
+alter table account add approved_at timestamp
+alter table account add approved_by varchar(20)
+
+
+
+/*------------create table admin----------*/
+create table admin(admin_id varchar(20), admin_password varchar(20))
+select * from admin
+insert into admin values('12345','@123')
 
 create table image(id int,user_image BLOB(65000))
 select * from image
+alter table image add email varchar(255)
 
 
 
