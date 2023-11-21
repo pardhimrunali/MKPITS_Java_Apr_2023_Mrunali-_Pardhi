cf560ba0b03c9590623f9fe7856b1d8ab8ca11fa
unknown <mrunali25pardhi@gmail.com>
2023-11-21 21:08:19 +0530

    Springboot_basic

---
+++ Java_programs/Advance_Java/springbootbasic/DemoFirstProjectnewApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoFirstProjectnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstProjectnewApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstProjectnewApplication.class, args);
+	}
+
+}
