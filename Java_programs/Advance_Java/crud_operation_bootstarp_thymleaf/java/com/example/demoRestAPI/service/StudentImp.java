f3765a799a60a684e79436222a2d7ee6dc38fc55
unknown <mrunali25pardhi@gmail.com>
2023-12-09 23:48:10 +0530

    bootstarp_crud_operation_using_thymleaf

---
+++ Java_programs/Advance_Java/crud_operation_bootstarp_thymleaf/java/com/example/demoRestAPI/service/StudentImp.java
@@ -0,0 +1,51 @@
+package com.example.demoRestAPI.service;
+
+import com.example.demoRestAPI.dao.RepsitoryJpaExample;
+import com.example.demoRestAPI.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Service
+public class StudentImp implements StudentService{
+    private RepsitoryJpaExample repsitoryJpaExample;
+@Autowired
+    public StudentImp(RepsitoryJpaExample repsitoryJpaExample) {
+        this.repsitoryJpaExample = repsitoryJpaExample;
+    }
+
+    @Override
+    @Transactional
+    public Student saveData(@ModelAttribute("stud") Student student) {
+        return repsitoryJpaExample.save(student);
+    }
+
+    @Override
+    @Transactional
+    public List<Student> showAllData() {
+        return repsitoryJpaExample.findAll();
+    }
+
+    @Override
+    public Student UpdateRecord(Student student) {
+        return repsitoryJpaExample.save(student);
+    }
+
+    @Override
+    public Student getRollNo(Integer roll_no) {
+        return repsitoryJpaExample.findById(roll_no).get();
+    }
+
+    @Override
+    public void deleteStudent(Integer roll_no) {
+        repsitoryJpaExample.deleteById(roll_no);
+    }
+
+
+}
+
+
+
